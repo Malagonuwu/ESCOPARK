7c6f04029a50a9dc0b32f75118a5b2ec21e3adfb
Luis David Martinez Martinez <ld.martinez.117@gmail.com>
2025-06-24 00:40:17 -0600

    HomeEstudiante

--- ESCOPARK-main/src/pages/Usuario/HomeEstudiante.jsx
+++ ESCOPARK-main/src/pages/Usuario/HomeEstudiante.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 
 // Imagen
 import fondo from "../../assets/Cel.jpg";
+import escomLogo from "../../assets/escom_logo.png";
+import escomFoto from "../../assets/escomFoto.jpeg";
 
 // Componentes
 import Header from "../../components/General/Header";
@@ -113,83 +115,204 @@ const Home = () => {
   };
 
   return (
-    <Container
-      disableGutters
-      sx={{
-        width: "412px",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
-    >
+    <Box sx={{width:"100%", height:"100vh"}}>
       {/* Header */}
       <Box>
-        <Header
-          sectionTitle="Inicio"
-          userName={userName}
-          showBackgroundImage={false}
-          backgroundColor="#002250"
-        />
+          <Header
+            sectionTitle="Inicio"
+            userName={userName}
+            showBackgroundImage={false}
+            backgroundColor="#002250"
+          />
       </Box>
-
-      {/* Zona comprobante */}
-      <Box sx={{ px: 2, py: 3 }}>
-        <Typography variant="h6" sx={{ mb: 2 }}>
-          Comprobante de horario
-        </Typography>
-
-        <input
-          type="file"
-          accept="application/pdf"
-          onChange={(e) => {
-            setSelectedFile(e.target.files[0]);
-            setFileError(null);
-          }}
-          style={{ display: 'block', marginBottom: '8px' }}
-        />
-        
-        {fileError && (
-          <Typography color="error" variant="body2" sx={{ mb: 1 }}>
-            {fileError}
+      <Container
+        disableGutters
+        sx={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column"
+        }}
+      >
+        <Box>
+          
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: "Orbitron, Sans-Serif",
+              fontWeight: "bold",
+              textAlign:"center",
+              fontSize: "22px",
+              mt:"1rem"
+            }}>
+            ¡Bienvenido Politécnico!
           </Typography>
-        )}
-
-        <Button
-          variant="contained"
-          onClick={handleUpload}
-          disabled={!selectedFile || uploading}
-          sx={{ mt: 1 }}
-        >
-          {uploading ? "Subiendo..." : (pdfUrl ? "Actualizar comprobante" : "Subir comprobante")}
-        </Button>
-
-        {pdfUrl && (
-          <Box sx={{ mt: 3 }}>
-            <Typography variant="subtitle1" sx={{ mb: 1 }}>
-              Comprobante actual:
-            </Typography>
-            <iframe
-              src={`${pdfUrl}#toolbar=0&navpanes=0&view=fitH`}
-              width="100%"
-              height="500px"
-              title="Comprobante de horario"
+          <Box 
+            sx={{ 
+              display: "flex", 
+              justifyContent: "center",
+              mt: "1rem"
+            }}
+          >
+            <img 
+              src= {escomLogo} // Reemplaza con la ruta correcta
+              alt="Logo Politécnico" 
               style={{ 
-                border: "1px solid #ccc",
-                borderRadius: "4px",
-                boxShadow: "0 2px 4px rgba(0,0,0,0.1)"
+                width: "15rem", 
+                objectFit:"contain"
               }}
             />
-            <Typography variant="caption" display="block" sx={{ mt: 1 }}>
-              Si no ves el PDF, <a href={pdfUrl} target="_blank" rel="noopener noreferrer">ábrelo en una nueva pestaña</a>
+          </Box>
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: "Orbitron, Sans-Serif",
+              fontSize: "14px",
+              textAlign:"center",
+              mt:"1rem"
+            }}>
+            Através de este sistema podrás gestionar tus vehículos que estacionas en el instituto.
+          </Typography>
+        </Box>
+
+        {/* Zona comprobante */}
+        <Box sx={{ px: 2, py: 3, display:"flex", flexDirection:"column", alignItems:"center" }}>
+          <Typography variant="h6" sx={{ mb: 2, fontFamily: "Orbitron, Sans-Serif",
+              fontWeight: "bold", textAlign:"center"}}>
+            Comprobante de horario
+          </Typography>
+
+          <input
+            type="file"
+            accept="application/pdf"
+            onChange={(e) => {
+              setSelectedFile(e.target.files[0]);
+              setFileError(null);
+            }}
+            style={{ display: 'block', marginBottom: '8px', fontFamily: "Orbitron, Sans-Serif"}}
+          />
+          
+          {fileError && (
+            <Typography color="error" variant="body2" sx={{ mb: 1, fontFamily: "Orbitron, Sans-Serif" }}>
+              {fileError}
+            </Typography>
+          )}
+
+          <Button
+            variant="contained"
+            onClick={handleUpload}
+            disabled={!selectedFile || uploading}
+            sx={{ mt: 1, fontFamily: "Orbitron, Sans-Serif"}}
+          >
+            {uploading ? "Subiendo..." : (pdfUrl ? "Actualizar comprobante" : "Subir comprobante")}
+          </Button>
+
+          {pdfUrl && (
+            <Box sx={{ mt: 3 }}>
+              <Typography variant="subtitle1" sx={{ mb: 1, fontFamily: "Orbitron, Sans-Serif" }}>
+                Comprobante actual:
+              </Typography>
+              <iframe
+                src={`${pdfUrl}#toolbar=0&navpanes=0&view=fitH`}
+                width="100%"
+                height="500px"
+                title="Comprobante de horario"
+                style={{ 
+                  border: "1px solid #ccc",
+                  borderRadius: "4px",
+                  boxShadow: "0 2px 4px rgba(0,0,0,0.1)"
+                }}
+              />
+              <Typography variant="caption" display="block" sx={{ mt: 1, fontFamily: "Orbitron, Sans-Serif" }}>
+                Si no ves el PDF, <a href={pdfUrl} target="_blank" rel="noopener noreferrer">ábrelo en una nueva pestaña</a>
+              </Typography>
+            </Box>
+          )}
+        </Box>
+        <Box 
+            sx={{ 
+              display: "flex", 
+              justifyContent: "center",
+              mt: "0rem"
+            }}
+          >
+            <img 
+              src= {escomFoto} // Reemplaza con la ruta correcta
+              alt="Foto Politécnico" 
+              style={{ 
+                width: "100%", 
+                height: "auto",
+                objectFit:"contain"
+              }}
+            />
+          </Box>
+        <Box sx={{ px: 2, py: 3, mb:"1rem" }}>
+          
+          <Box sx={{ display: 'flex', alignItems: 'flex-start', mb: '1rem' }}>
+            <Box sx={{
+              width: '6px',
+              height: '6px',
+              backgroundColor: 'currentColor',
+              borderRadius: '50%',
+              mt: '9px', // Ajusta para alinear verticalmente con el texto
+              mr: '8px',
+              flexShrink: 0
+            }} />
+            <Typography
+              variant="h6"
+              sx={{
+                fontFamily: "Orbitron, Sans-Serif",
+                fontWeight: "regular",
+                fontSize: "15px",
+              }}>
+              Registra tus vehículos para un acceso rápido y eficiente.
             </Typography>
           </Box>
-        )}
-      </Box>
+          <Box sx={{ display: 'flex', alignItems: 'flex-start', mb: '1rem' }}>
+            <Box sx={{
+              width: '6px',
+              height: '6px',
+              backgroundColor: 'currentColor',
+              borderRadius: '50%',
+              mt: '9px', // Ajusta para alinear verticalmente con el texto
+              mr: '8px',
+              flexShrink: 0
+            }} />
+            <Typography
+              variant="h6"
+              sx={{
+                fontFamily: "Orbitron, Sans-Serif",
+                fontWeight: "regular",
+                fontSize: "15px",
+              }}>
+              Gestiona la entrada y salidad de tus vehículos.
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'flex-start', mb: '1rem' }}>
+            <Box sx={{
+              width: '6px',
+              height: '6px',
+              backgroundColor: 'currentColor',
+              borderRadius: '50%',
+              mt: '9px', // Ajusta para alinear verticalmente con el texto
+              mr: '8px',
+              flexShrink: 0
+            }} />
+            <Typography
+              variant="h6"
+              sx={{
+                fontFamily: "Orbitron, Sans-Serif",
+                fontWeight: "regular",
+                fontSize: "15px",
+              }}>
+              Conoce tus estadísticas y edita tu perfil.
+            </Typography>
+          </Box>
+        </Box>
 
+      </Container>
       {/* Navegación */}
-      <NavegationBar />
-    </Container>
+      <NavegationBar sx={{justifyContent:"flex-end"}}/>
+    </Box>
   );
 };
 
